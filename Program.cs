6002da6e0b35b0b7be8bc50e419a98e99eb9bfe8
larahbsantos <larahbsb06@gmail.com>
2025-09-21 19:12:13 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("São Vicente");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Santos");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruibe");
